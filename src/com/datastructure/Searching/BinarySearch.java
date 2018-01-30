6d61f2ed67a3045dd7d1e95a768a183fc15d01f1
Srinivas Gudipati <srinivasgudipati321@gmail.com>
2018-01-29 21:28:54 -0500

    commit

---
+++ src/com/datastructure/Searching/BinarySearch.java
@@ -0,0 +1,13 @@
+package com.datastructure.Searching;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {10,2,4,1,5,7,8};
+		int search=5;
+
+	}
+
+}
